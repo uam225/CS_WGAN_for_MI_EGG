bd042ef8fc9d2d3139eddcedc2c417410790726b
uam225 <75205469+uam225@users.noreply.github.com>
2024-04-19 08:35:58 +0100

    Update data_loader.py_removed unused code

--- data_loader.py
+++ data_loader.py
@@ -14,13 +14,13 @@ class EEGFeatureDataset(Dataset):
                 data_path = os.path.join(data_dir, f'sub-{subj:03d}_ses-{sess:02d}_task_motorimagery_eeg.mat')
                 feature_path = os.path.join(feature_dir, f'csp_features_sub{subj}_sess{sess}.mat')
 
-                # Load EEG data
+                #load EEG data
                 mat_data = sio.loadmat(data_path)
                 all_data = mat_data['data']
                 cz_index, c3_index, c4_index = 11, 12, 13
                 data = all_data[:, [cz_index, c3_index, c4_index], :]
 
-                # Load features
+                #load features
                 mat_features = sio.loadmat(feature_path)
                 features = mat_features['csp_features']
 
@@ -28,22 +28,9 @@ class EEGFeatureDataset(Dataset):
                 eeg_shape = data.shape[1] * data.shape[2]
                 features_no = features.shape[1]
 
-                '''for trial in range(min_trials):
-                    #normalise each EEG data trial using z-score normalization
-                    trial_data = data[trial]
-                    trial_mean = np.mean(trial_data)
-                    trial_std = np.std(trial_data)
-                    normalized_trial_data = (trial_data - trial_mean) / trial_std
-                    flattened_data = normalized_trial_data.flatten()
-                    self.data_samples.append(flattened_data)
-
-                    #normalise each feature vector (already using z-score normalization)
-                    trial_features = features[trial]
-                    trial_features = (trial_features - np.mean(trial_features)) / np.std(trial_features)
-                    self.feature_samples.append(trial_features)'''
 
                 for trial in range(min_trials):
-                    #normalize each EEG data trial
+                    #normalise each EEG data trial
                     trial_data = data[trial]
                     trial_data_min = np.min(trial_data)
                     trial_data_max = np.max(trial_data)
@@ -51,7 +38,7 @@ class EEGFeatureDataset(Dataset):
                     flattened_data = normalized_trial_data.flatten()
                     self.data_samples.append(flattened_data)
 
-                    #normalize each feature vector
+                    #normalise each feature vector
                     trial_features = features[trial]
                     trial_features = (trial_features - np.mean(trial_features)) / np.std(trial_features)
                     self.feature_samples.append(trial_features)
@@ -116,37 +103,3 @@ class EEGFeatureDataset(Dataset):
         data_sample = torch.tensor(self.data_samples[idx], dtype=torch.float32)  # Convert to float32
         feature_sample = torch.tensor(self.feature_samples[idx], dtype=torch.float32)  # Convert to float32
         return data_sample, feature_sample'''
-
-
-
-#unused code
-'''
-data_dir = '/Users/umairarshad/SHU/or_data/mat'
-feature_dir = '/Users/umairarshad/SHU/or_data/features'  
-subjects = range(6, 11)  
-sessions = range(1, 6)  
-
-dataset = EEGFeatureDataset(data_dir, feature_dir, subjects, sessions)
-data_loader = DataLoader(dataset, batch_size=32, shuffle=True)
-
-type(dataset)
-
-mat_data = sio.loadmat('/Users/umairarshad/SHU/or_data/mat/sub-001_ses-01_task_motorimagery_eeg.mat')
-data = mat_data['data']
-print(data.shape)
-
-features = np.load(feature_dir + '/sub_4_ses_1_fold_1_train_features.npy')
-features.shape
-min_trials = min(data.shape[0], features.shape[0]) 
-data_samples_check = np.zeros((min_trials,32000))#[]
-features_samples_check = np.zeros((min_trials,10))#[]
-
-for trial in range(min_trials):
-    flattened_data = data[trial].flatten()
-    data_samples_check[trial,:] = flattened_data
-    features_samples_check[trial, :] = features[trial,:]
-    #data_samples_check.append(flattened_data)
-    #features_samples_check.append(features[trial])
-print(flattened_data.shape)
-print(data_samples_check.shape)
-print(features_samples_check.shape)'''
